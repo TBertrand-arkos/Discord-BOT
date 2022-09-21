821e019f57d3e19dcdc0201f256384efbb0b2626
TBertrand_arkos <bertrand.tarascon@epita.fr>
2022-09-21 19:32:38 +0200

    [ADD][CMD] name & d6 working

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import random
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,7 +11,7 @@ bot = commands.Bot(
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = 226250876338176002  # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -21,5 +22,14 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx):
+    await ctx.send(ctx.author)
+
+@bot.command()
+async def d6(ctx):
+    pos = [1, 2, 3, 4, 5, 6]
+    await ctx.send(random.choice(pos))
+
+token = ""
 bot.run(token)  # Starts the bot
